7d93158e47355dccf30113b5cdbc12440d41985c
yixuehan <505703394@qq.com>
2019-08-30 19:02:43 +0800

    update

--- mako/go/restful/init_restful.go
+++ mako/go/restful/init_restful.go
@@ -8,5 +8,5 @@ import (
 
 func InitRestful() {
 	<% import os %>
-	${os.path.basename(package_restful_api_dir)}.Run(fmt.Sprintf("%v:%v", define.Cfg.RestfulAddr.Ip, define.Cfg.RestfulAddr.Port))
+	${os.path.basename(package_restful_api_dir)}.Run(fmt.Sprintf("%v:%v", define.Cfg.HttpAddr.Ip, define.Cfg.HttpAddr.Port))
 }
