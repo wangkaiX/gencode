6240bfd7475d89f4fab17809d8828fd21460ac03
yixuehan <505703394@qq.com>
2019-09-09 16:08:28 +0800

    optimize field name

--- mako/go/restful/api.go
+++ mako/go/restful/api.go
@@ -5,15 +5,15 @@ import "${package_restful_define_dir}"
 import "${package_project_dir}/app/errno"
 import "github.com/gin-gonic/gin"
 
-func ${gen_lower_camel(api.name)}(ctx *gin.Context, ${api.url_param.name} *${os.path.basename(restful_define_dir)}.${api.url_param.type.name}, ${api.req.name} *${os.path.basename(restful_define_dir)}.${api.req.type.name})(${api.resp.name} *${os.path.basename(restful_define_dir)}.${api.resp.type.name}) {
-    ${api.resp.name} = &${os.path.basename(restful_define_dir)}.${api.resp.type.name}{}
+func ${gen_lower_camel(api.name)}(ctx *gin.Context, urlParam *${os.path.basename(restful_define_dir)}.${api.url_param.type.name}, req *${os.path.basename(restful_define_dir)}.${api.req.type.name})(resp *${os.path.basename(restful_define_dir)}.${api.resp.type.name}) {
+    resp = &${os.path.basename(restful_define_dir)}.${api.resp.type.name}{}
     var ec *errno.Error
     defer func() {
         if ec == nil {
             ec = errno.GenSuccess()
         }
-        ${api.resp.name}.Code = ec.Code
-        ${api.resp.name}.Msg = ec.Msg
+        resp.Code = ec.Code
+        resp.Msg = ec.Msg
     }()
 
     // code here
