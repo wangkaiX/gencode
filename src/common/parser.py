35c7d60798f08f40ad5611544671c04fade84c75
desert <505703394@qq.com>
2019-11-01 09:56:36 +0800

    save

--- src/common/parser.py
+++ src/common/parser.py
@@ -4,6 +4,8 @@
 from gencode.common import tool
 from gencode.common import meta
 import util.python.util as util
+from abc import abstractmethod
+import json5
 
 
 type_enum = 'ENUM'
@@ -169,8 +171,37 @@ class Parser:
         self.filename = filename
         self.fp = fp
         self.text = text
+        print("base parser")
+
+    def parser(self):
+        if self.filename:
+            self.parser_file()
+        elif self.fp:
+            self.parser_fp()
+        return self.parser_text()
+
+    def parser_file(self):
+        self.fp = open(self.filename, "rb")
+        self.parser_fp()
+
+    def parser_fp(self):
+        self.text = self.fp.read()
+        self.parser_text()
+
+    @abstractmethod
+    def parser_text(self):
+        assert False
+
+    def parser_dict(self):
+        assert False
 
 
-class Json(Parser):
+class Json5(Parser):
     def __init__(self, filename, fp, text):
-        self.Parser.__init__(self, filename, fp, text)
+        Parser.__init__(self, filename, fp, text)
+
+    def parser_text(self):
+        return json5.loads(self.text)
+
+
+class Apis(Parser):
