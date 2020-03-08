0abe85351ffe8556ff9a58ece8abff0e775a9b63
desert <505703394@qq.com>
2020-03-08 21:39:02 +0800

    save

--- code_framework/common/doc.py
+++ code_framework/common/doc.py
@@ -2,24 +2,21 @@
 # -*- coding: utf-8 -*-
 
 from src.common import tool
-import os
 
 
 # 接口文档
 class Doc:
-    def __init__(self, mako_file, doc_name, doc_dir, apis, enums, errnos):
+    def __init__(self, mako_file, doc_name, apis, enums, errnos):
         self.__apis = apis
         self.__doc_name = doc_name
-        self.__doc_dir = doc_dir
         self.__mako_file = mako_file
         self.__enums = enums
         self.__errnos = errnos
 
-    def __gen_tag_doc(self, apis):
-        doc_name = os.path.join(self.__doc_dir, self.__doc_name)
+    def gen_doc(self, apis):
         tool.gen_code_file(self.__mako_file,
-                           doc_name,
-                           apis=apis,
+                           self.__doc_name,
+                           apis=self.__apis,
                            dict2json=tool.dict2json,
                            url_param2text=tool.url_param2text,
                            markdown_full_path=tool.markdown_full_path,
